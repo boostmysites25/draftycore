733f0ad91a7f6e782d0b6e86096930c7c5d25d6a
Pranav <mpranavprem@gmail.com>
2026-02-19 12:56:43 +0530

    changes

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { FaArrowLeft } from 'react-icons/fa';
 
 const Login = () => {
     const [email, setEmail] = useState('');
