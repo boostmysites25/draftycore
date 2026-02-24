994a1d4b8a845d098a1786e2daf24d9198ca00ef
Pranav <mpranavprem@gmail.com>
2026-02-24 16:12:39 +0530

    section: impact/marketing completed

--- src/pages/Marketing.tsx
+++ src/pages/Marketing.tsx
@@ -1,16 +1,19 @@
 import { lazy } from "react";
+import SmoothScroll from "../components/common/SmoothScroll";
 
 const Hero = lazy(() => import("../components/home/Hero"));
 const WhoWeAre = lazy(() => import("../components/marketing/WhoWeAre"));
 const Steps = lazy(() => import("../components/marketing/Steps"));
+const Impact = lazy(() => import("../components/marketing/Impact"));
 
 const Marketing = () => {
   return (
-    <>
+    <SmoothScroll>
       <Hero delay={0} />
       <WhoWeAre />
       <Steps />
-    </>
+      <Impact />
+    </SmoothScroll>
   );
 };
 
